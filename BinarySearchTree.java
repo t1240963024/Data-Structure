61f8f33ed5c52538445434e8be85cbe7b9c7ab25
t1240963024 <1240963024@qq.com>
2017-08-16 13:41:12 +1000

    Add files via upload

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,7 +1,29 @@
 package algorithm;
 
 public class BinarySearchTree<T extends Comparable<T>> {
-	
+	/*
+	 * 二叉搜索树：
+	 * 父节点的左子树上的所有点的值都小于父节点的值
+	 * 父节点的右子树上的所有点的值都大于父节点的值
+	 * 键值均不相等
+	 * 任意左右子树也为二叉搜索树
+	 * 
+	 * 插入
+	 * 1.如果根节点为空 则新插入节点为根节点
+	 * 2.如果根节点不为空 则从根节点开始对比要插入的节点的键值new.value和当前节点键值current.value
+	 * 3.如果新插入的节点键值比当前节点小
+	 *   3.1 如果当前节点的左子树为空 则新插入节点为当前节点的左孩子
+	 *   3.2 否则current=current.left 返回步骤2
+	 * 4.如果新插入节点键值比当前节点大
+	 *   4.1 如果当前节点的右子树为空 则新插入节点为当前节点右孩子
+	 *   4.2 否则current=current.right 返回步骤2
+	 * 
+	 * 删除
+	 * 1.如果要删除的节点既没有左孩子也没有右孩子 则直接删除
+	 * 2.如果要删除的节点有左孩子没有右孩子 则其左孩子替换为被删除的节点
+	 * 3.如果要删除的节点有右孩子没有左孩子 则其右孩子替换为被删除的节点
+	 * 3.如果要删除的节点既有左孩子也有右孩子 则找出其后继节点 替换被删除的节点
+	 * */
 	@SuppressWarnings("hiding")
 	class Node<T> {
 		private Node<T> left;
@@ -36,8 +58,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		//根节点不为空
 		while(currentNode != null) {
-			//放到左子树
-			if(currentNode.key.compareTo(newNode.key) > 0) {
+			if(currentNode.key.compareTo(newNode.key) > 0) {//放到左子树
 				if(currentNode.left == null) {
 					currentNode.left = newNode;
 					newNode.parent = currentNode;
@@ -45,7 +66,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 				}else {
 					currentNode = currentNode.left;
 				}
-			}else {	//放到右子树
+			}else if(currentNode.key.compareTo(newNode.key) < 0){	//放到右子树
 				if(currentNode.right == null) {
 					currentNode.right = newNode;
 					newNode.parent = currentNode;
@@ -53,10 +74,30 @@ public class BinarySearchTree<T extends Comparable<T>> {
 				}else {
 					currentNode = currentNode.right;
 				}
-			}		
+			}else {//数据重复则报错
+				System.err.println("no duplicate value");
+			}
 		}		
 	}
-
+	//前驱 返回前驱结点 即该节点左子书中最大的节点
+	public Node<T> predecessor(Node<T> node) {
+		if(node.left != null) {
+			Node<T> temp = node.left;
+			while(temp.right != null) {
+				temp = temp.right;
+			}
+			return temp;
+		}
+		
+		Node<T> parent = node.parent;
+		while((parent != null) && (node == parent.left)) {
+			node = parent;
+			parent = parent.parent;
+		}
+		
+		return parent;
+	}
+	//后继 返回后继节点 即该节点右子树中最小的节点
 	public Node<T> successor(Node<T> node) {
 		if(node.right != null) {
 			Node<T> temp = node.right;
@@ -75,6 +116,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		return parent;
 		
 	}
+	//查找结点 返回找到的节点
 	public Node<T> search(Node<T> node, T value) {
 		if(node == null) {
 			return node;
@@ -88,8 +130,8 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		
 	}
-	
-	public void delete(Node<T> node) {
+	//删除节点 内部调用
+	private void delete(Node<T> node) {
 		if(node == null) {
 			return;
 		}
@@ -130,7 +172,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		delete(succNode);
 		node.key = succNode.key;
 	}
-		
+	//删除节点 外部调用
 	public void delete(T item) {
 		// TODO Auto-generated method stub
 		Node<T> node = search(mRoot, item);
@@ -140,14 +182,29 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			delete(node);
 		}
 	}
-	
+	//后序遍历
+	public void postOrder(Node<T> node) {
+		if(node != null) {
+			System.out.println(node.key);
+			postOrder(node.left);
+			postOrder(node.right);
+		}
+	}
+	//中序遍历
+	public void inOrder(Node<T> node) {
+		if(node != null) {
+			inOrder(node.left);
+			System.out.println(node.key);
+			inOrder(node.right);
+		}
+	}
+	//前序遍历
 	public void preOrder(Node<T> node) {
 		if(node != null) {
 			System.out.println(node.key);
 			preOrder(node.left);
 			preOrder(node.right);
 		}
-
 	}
 	
 	public void print() {
